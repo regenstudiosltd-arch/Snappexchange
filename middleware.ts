33a80fcf17b93b6594fc71c17a7c183a2abf1f7c
kessie2862 <kayp3477@gmail.com>
2026-04-03 11:14:49 +0000

    feat: add forgot-password in matcher

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,7 @@ export default withAuth({
 });
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|signup|otp|$).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|signup|otp|forgot-password|$).*)',
+  ],
 };
