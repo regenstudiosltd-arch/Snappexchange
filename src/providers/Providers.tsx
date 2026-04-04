3bb6829a4cfb1d7279a9d74c8874a10a3a7f374a
kessie2862 <kayp3477@gmail.com>
2026-04-04 17:13:47 +0000

    fix: remove comment

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,4 +1,5 @@
 // src/providers/Providers.tsx
+
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -31,34 +32,3 @@ export function Providers({ children }: { children: React.ReactNode }) {
     </SessionProvider>
   );
 }
-
-// // src/providers/Providers.tsx
-
-// 'use client';
-
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { SessionProvider } from 'next-auth/react';
-// import { useState } from 'react';
-// import { ThemeProvider } from '@/src/context/ThemeContext';
-
-// export function Providers({ children }: { children: React.ReactNode }) {
-//   const [queryClient] = useState(
-//     () =>
-//       new QueryClient({
-//         defaultOptions: {
-//           queries: {
-//             refetchOnWindowFocus: false,
-//             retry: 1,
-//           },
-//         },
-//       }),
-//   );
-
-//   return (
-//     <SessionProvider>
-//       <QueryClientProvider client={queryClient}>
-//         <ThemeProvider>{children}</ThemeProvider>
-//       </QueryClientProvider>
-//     </SessionProvider>
-//   );
-// }
