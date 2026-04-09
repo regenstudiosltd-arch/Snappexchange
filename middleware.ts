51f76f362e1b67b08d7126681edcc409d3ccf6f1
kessie2862 <kayp3477@gmail.com>
2026-04-09 11:48:42 +0000

    fix: update middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,14 @@ export default withAuth({
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|signup|otp|forgot-password|invite|$).*)',
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - The public pages: signup, otp, faq, terms, etc.
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|signup|otp|forgot-password|invite|faq|terms|privacy|security|$).*)',
   ],
 };
